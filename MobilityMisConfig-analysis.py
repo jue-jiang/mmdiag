c7663360f2c7674332bc9d0a3fd46c11bfa179d6
Haotian Deng <moonsky219@live.cn>
2017-10-10 15:33:59 -0400

    Fix Mobility entry function

--- MobilityMisConfig-analysis.py
+++ MobilityMisConfig-analysis.py
@@ -17,18 +17,19 @@ def get_sorted_logs(directory):
     for root, d, filenames in os.walk(directory):
         logs = [f for f in filenames if f.endswith(".mi2log")]
         break
-    sorted_logs = []
-    for f in logs:
-        # print f
-        # location, rat, d, t, imsi, phone, carrier = re.match(r"(.+)_(.+)_diag_log_(\d+)_(\d+)_(\d+|null)_(samsung-SM-G900T|Huawei-Nexus6P|Google-Pixel)_(T-Mobile|null|AT&T-MicroCell|AT&T|VerizonWireless|Sprint)\.mi2log", f).groups()
-        d, t, imsi, phone, carrier = re.match(r"diag_log_(\d+)_(\d+)_(\d+|null)_(samsung-SM-G900T|Huawei-Nexus6P|Google-Pixel|Google-PixelXL)_(T-Mobile|null|AT&T-MicroCell|AT&T|VerizonWireless|Sprint|Project_Fi-310260|-)\.mi2log", f).groups()
-        if len(t) < 6:
-            t += '0'
-        key = d + t
-        sorted_logs.append((key, f))
-    # sorted by date + time
-    sorted_logs = [f for k, f in sorted(sorted_logs)]
-    return sorted_logs
+    # sorted_logs = []
+    # for f in logs:
+    #     # print f
+    #     # location, rat, d, t, imsi, phone, carrier = re.match(r"(.+)_(.+)_diag_log_(\d+)_(\d+)_(\d+|null)_(samsung-SM-G900T|Huawei-Nexus6P|Google-Pixel)_(T-Mobile|null|AT&T-MicroCell|AT&T|VerizonWireless|Sprint)\.mi2log", f).groups()
+    #     d, t, imsi, phone, carrier = re.match(r"diag_log_(\d+)_(\d+)_(\d+|null)_(samsung-SM-G900T|Huawei-Nexus6P|Google-Pixel|Google-PixelXL)_(T-Mobile|null|AT&T-MicroCell|AT&T|VerizonWireless|Sprint|Project_Fi-310260|-)\.mi2log", f).groups()
+    #     if len(t) < 6:
+    #         t += '0'
+    #     key = d + t
+    #     sorted_logs.append((key, f))
+    # # sorted by date + time
+    # sorted_logs = [f for k, f in sorted(sorted_logs)]
+    # return sorted_logs
+    return logs
 
 
 def parse_qmdl(path_input):
@@ -43,11 +44,125 @@ def parse_qmdl(path_input):
 
     for f in logs:
         # print "=== %s ===" % f
-        src.set_input_path(directory + f)
+        m.reset()
         m.set_source(src)
+        src.set_input_path(directory + f)
 
         # Start the monitoring
         src.run()
+
+        umts_normal_service_log = m.get_umts_normal_service_log()
+        umts_plmn_search_log = m.get_umts_plmn_search_log()
+        umts_attach_log = m.get_umts_attach_log()
+        umts_lu_log = m.get_umts_lu_log()
+        umts_rau_log = m.get_umts_rau_log()
+
+        lte_normal_service_log = m.get_lte_normal_service_log()
+        lte_plmn_search_log = m.get_lte_plmn_search_log()
+        lte_attach_log = m.get_lte_attach_log()
+        lte_tau_log = m.get_lte_tau_log()
+        lte_tau_qos_info = m.get_lte_tau_qos_info()
+        lte_cell_resel_to_umts_config = m.get_lte_cell_resel_to_umts_config()
+        lte_drx_config = m.get_lte_drx_config()
+        lte_tdd_config = m.get_lte_tdd_config()
+        n_lte_rrc_reconfig = m.get_n_lte_rrc_reconfig()
+        lte_mobility_misconfig_serving_cell_dict = m.get_lte_mobility_misconfig_serving_cell_dict()
+        utra_mobility_misconfig_serving_cell_dict = m.get_3g_mobility_misconfig_serving_cell_dict()
+
+        for CellIdentityCombine, CellInfo in lte_mobility_misconfig_serving_cell_dict.iteritems():
+            print CellIdentityCombine
+            InfoKey = "lte_rrc_cerv_cell_info"
+            if InfoKey in CellInfo:
+                print InfoKey
+                for InfoValueItem in CellInfo[InfoKey]:
+                    print InfoValueItem
+            InfoKey = "[sib3]cellReselectionInfoCommon_element"
+            if InfoKey in CellInfo:
+                print InfoKey
+                for InfoValueItem in CellInfo[InfoKey]:
+                    print InfoValueItem
+            InfoKey = "[sib3]cellReselectionServingFreqInfo_element"
+            if InfoKey in CellInfo:
+                print InfoKey
+                for InfoValueItem in CellInfo[InfoKey]:
+                    print InfoValueItem
+            InfoKey = "[sib3]intraFreqCellReselectionInfo_element"
+            if InfoKey in CellInfo:
+                print InfoKey
+                for InfoValueItem in CellInfo[InfoKey]:
+                    print InfoValueItem
+            InfoKey = "[sib4]intraFreqBlackCellList"
+            if InfoKey in CellInfo:
+                print InfoKey
+                for InfoValueItem in CellInfo[InfoKey]:
+                    print InfoValueItem
+            InfoKey = "[sib5]InterFreqCarrierFreqInfo_element"
+            if InfoKey in CellInfo:
+                print InfoKey
+                for InfoValueItem in CellInfo[InfoKey]:
+                    print InfoValueItem
+            InfoKey = "[sib5]interFreqBlackCellList"
+            if InfoKey in CellInfo:
+                print InfoKey
+                for InfoValueItem in CellInfo[InfoKey]:
+                    print InfoValueItem
+            InfoKey = "[sib6]CarrierFreqUTRA_FDD_element"
+            if InfoKey in CellInfo:
+                print InfoKey
+                for InfoValueItem in CellInfo[InfoKey]:
+                    print InfoValueItem
+            InfoKey = "[sib8]"
+            if InfoKey in CellInfo:
+                print InfoKey
+                for InfoValueItem in CellInfo[InfoKey]:
+                    print InfoValueItem
+            InfoKey = "lte_rrc_measurement_report"
+            if InfoKey in CellInfo:
+                print InfoKey
+                for InfoValueItem in CellInfo[InfoKey]:
+                    print InfoValueItem
+            InfoKey = "lte_measurement_report_config"
+            if InfoKey in CellInfo:
+                print InfoKey
+                for InfoValueItem in CellInfo[InfoKey]:
+                    print InfoValueItem
+            InfoKey = "lte_measurement_object"
+            if InfoKey in CellInfo:
+                print InfoKey
+                for InfoValueItem in CellInfo[InfoKey]:
+                    print InfoValueItem
+            InfoKey = "utra_3g_measurement_object"
+            if InfoKey in CellInfo:
+                print InfoKey
+                for InfoValueItem in CellInfo[InfoKey]:
+                    print InfoValueItem
+            InfoKey = "lte_report_configuration"
+            if InfoKey in CellInfo:
+                print InfoKey
+                for InfoValueItem in CellInfo[InfoKey]:
+                    print InfoValueItem
+            InfoKey = "2g3g_report_reconfiguration"
+            if InfoKey in CellInfo:
+                print InfoKey
+                for InfoValueItem in CellInfo[InfoKey]:
+                    print InfoValueItem
+
+            print "-----------------------------------------------------------"
+
+        for CellIdentityCombine, CellInfo in utra_mobility_misconfig_serving_cell_dict.iteritems():
+            print CellIdentityCombine
+            for InfoKey, InfoValue in CellInfo.iteritems():
+                print InfoKey
+                if isinstance(InfoValue, list):
+                    for InfoValueItem in InfoValue:
+                        print InfoValueItem
+                elif isinstance(InfoValue, dict):
+                    print "\n\n\n\nHaotian\n\n\n\n"
+                    for InfoValueKey, InfoValueItem in InfoValue:
+                        print InfoValueKey
+                        print InfoValueItem
+            print "-----------------------------------------------------------"
+
 
     # fd = open("mmAnalyzer-result.pickle", "wb")
     # pickle.dump(m.get_umts_normal_service_log(), fd)
@@ -71,117 +186,6 @@ def parse_qmdl(path_input):
     # fd.flush()
     # fd.close()
 
-    umts_normal_service_log = m.get_umts_normal_service_log()
-    umts_plmn_search_log = m.get_umts_plmn_search_log()
-    umts_attach_log = m.get_umts_attach_log()
-    umts_lu_log = m.get_umts_lu_log()
-    umts_rau_log = m.get_umts_rau_log()
-
-    lte_normal_service_log = m.get_lte_normal_service_log()
-    lte_plmn_search_log = m.get_lte_plmn_search_log()
-    lte_attach_log = m.get_lte_attach_log()
-    lte_tau_log = m.get_lte_tau_log()
-    lte_tau_qos_info = m.get_lte_tau_qos_info()
-    lte_cell_resel_to_umts_config = m.get_lte_cell_resel_to_umts_config()
-    lte_drx_config = m.get_lte_drx_config()
-    lte_tdd_config = m.get_lte_tdd_config()
-    n_lte_rrc_reconfig = m.get_n_lte_rrc_reconfig()
-    lte_mobility_misconfig_serving_cell_dict = m.get_lte_mobility_misconfig_serving_cell_dict()
-    utra_mobility_misconfig_serving_cell_dict = m.get_3g_mobility_misconfig_serving_cell_dict()
-
-    for CellIdentityCombine, CellInfo in lte_mobility_misconfig_serving_cell_dict.iteritems():
-        print CellIdentityCombine
-        InfoKey = "lte_rrc_cerv_cell_info"
-        if InfoKey in CellInfo:
-            print InfoKey
-            for InfoValueItem in CellInfo[InfoKey]:
-                print InfoValueItem
-        InfoKey = "[sib3]cellReselectionInfoCommon_element"
-        if InfoKey in CellInfo:
-            print InfoKey
-            for InfoValueItem in CellInfo[InfoKey]:
-                print InfoValueItem
-        InfoKey = "[sib3]cellReselectionServingFreqInfo_element"
-        if InfoKey in CellInfo:
-            print InfoKey
-            for InfoValueItem in CellInfo[InfoKey]:
-                print InfoValueItem
-        InfoKey = "[sib3]intraFreqCellReselectionInfo_element"
-        if InfoKey in CellInfo:
-            print InfoKey
-            for InfoValueItem in CellInfo[InfoKey]:
-                print InfoValueItem
-        InfoKey = "[sib4]intraFreqBlackCellList"
-        if InfoKey in CellInfo:
-            print InfoKey
-            for InfoValueItem in CellInfo[InfoKey]:
-                print InfoValueItem
-        InfoKey = "[sib5]InterFreqCarrierFreqInfo_element"
-        if InfoKey in CellInfo:
-            print InfoKey
-            for InfoValueItem in CellInfo[InfoKey]:
-                print InfoValueItem
-        InfoKey = "[sib5]interFreqBlackCellList"
-        if InfoKey in CellInfo:
-            print InfoKey
-            for InfoValueItem in CellInfo[InfoKey]:
-                print InfoValueItem
-        InfoKey = "[sib6]CarrierFreqUTRA_FDD_element"
-        if InfoKey in CellInfo:
-            print InfoKey
-            for InfoValueItem in CellInfo[InfoKey]:
-                print InfoValueItem
-        InfoKey = "[sib8]"
-        if InfoKey in CellInfo:
-            print InfoKey
-            for InfoValueItem in CellInfo[InfoKey]:
-                print InfoValueItem
-        InfoKey = "lte_rrc_measurement_report"
-        if InfoKey in CellInfo:
-            print InfoKey
-            for InfoValueItem in CellInfo[InfoKey]:
-                print InfoValueItem
-        InfoKey = "lte_measurement_report_config"
-        if InfoKey in CellInfo:
-            print InfoKey
-            for InfoValueItem in CellInfo[InfoKey]:
-                print InfoValueItem
-        InfoKey = "lte_measurement_object"
-        if InfoKey in CellInfo:
-            print InfoKey
-            for InfoValueItem in CellInfo[InfoKey]:
-                print InfoValueItem
-        InfoKey = "utra_3g_measurement_object"
-        if InfoKey in CellInfo:
-            print InfoKey
-            for InfoValueItem in CellInfo[InfoKey]:
-                print InfoValueItem
-        InfoKey = "lte_report_configuration"
-        if InfoKey in CellInfo:
-            print InfoKey
-            for InfoValueItem in CellInfo[InfoKey]:
-                print InfoValueItem
-        InfoKey = "2g3g_report_reconfiguration"
-        if InfoKey in CellInfo:
-            print InfoKey
-            for InfoValueItem in CellInfo[InfoKey]:
-                print InfoValueItem
-
-        print "-----------------------------------------------------------"
-
-    for CellIdentityCombine, CellInfo in utra_mobility_misconfig_serving_cell_dict.iteritems():
-        print CellIdentityCombine
-        for InfoKey, InfoValue in CellInfo.iteritems():
-            print InfoKey
-            if isinstance(InfoValue, list):
-                for InfoValueItem in InfoValue:
-                    print InfoValueItem
-            elif isinstance(InfoValue, dict):
-                print "\n\n\n\nHaotian\n\n\n\n"
-                for InfoValueKey, InfoValueItem in InfoValue:
-                    print InfoValueKey
-                    print InfoValueItem
-        print "-----------------------------------------------------------"
 
 def map_plmn(name):
     d = {   "": "Unknown",
